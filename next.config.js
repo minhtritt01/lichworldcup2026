3dfae38201e1996e5b30b5557d9796c135b07a78
zeno <tri.1030@enotion.io>
2026-06-10 21:23:00 +0700

    fixed: sitemap locale

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  async rewrites() {
+    return [
+      {
+        source: '/en/sitemap.xml',
+        destination: '/sitemap.xml',
+      },
+    ];
+  },
 };
 
 module.exports = withNextIntl(nextConfig);
